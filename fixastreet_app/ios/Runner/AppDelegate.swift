279f446444be23d4eb4ae10e5c7a5a083ccc0efc
macilah-carter <museemacilah@gmail.com>
2025-07-27 15:30:31 +0300

    env

--- fixastreet_app/ios/Runner/AppDelegate.swift
+++ fixastreet_app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import Flutter
 import UIKit
 import GoogleMaps
+import Dotenv
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +9,11 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCy1HUoprYzHv4a8t0EbDLkM_klQhiONXo")
+    if let googleKey = Dotenv().load()["GOOGLE_KEY"] {
+        GMSServices.provideAPIKey(googleKey)
+    } else {
+        fatalError("Google API key not found in .env file")
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- fixastreet_app/lib/main.dart
+++ fixastreet_app/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:fixastreet_app/pages/navigation_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  // Load environment variables from .env file
+  await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
 
@@ -22,11 +26,8 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      routes: {
-        '/': (context) => const NavigationPage(),
-      },
+      routes: {'/': (context) => const NavigationPage()},
       // home: NavigationPage(),
     );
   }
 }
-
